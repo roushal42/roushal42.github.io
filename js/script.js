415805d4b5ca5e0e08877abb36ae8a80d4ebed0c
roushlm23 <roushal42@gmail.com>
2025-05-09 16:55:10 -0400

    bgyfyu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ Schedule.onclick = function () {
     alert("You selected schedule!");
 };
 
+let wheel = document.querySelector('.wheel');
+let spinBtn = document.querySelector('.spinBtn');
+let value = Math.ceil(Math.random() * 3600);
+
+spinBtn.onclick = function() {
+  wheel.style.transform = "rotate(" + value + "deg)";
+  value += Math.ceil(Math.random() * 3600);
+}
